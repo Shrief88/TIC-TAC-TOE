bb756afbf830104c65e59eedebfa4f833422ec29
Shrief Essam <shriefessam1999@gmail.com>
2022-07-11 01:42:01 +0200

    organise the code as module pattern

--- main.js
+++ main.js
@@ -1,54 +1,108 @@
-const grid = document.querySelector('.grid');
-const arrayGrid = Array.prototype.slice.call(document.querySelectorAll('.cell'));
-let isPlayerOne = true;
-let gameboard = [0,0,0,0,0,0,0,0,0];
-const result = document.querySelector('#result');
+const Gameboard = (()=>{
+    let gameboard = [0,0,0,0,0,0,0,0,0];
 
+    const setField = (index,sign)=>{
+        gameboard[index]= sign;
+    }
 
-grid.addEventListener('click',(e)=>{
-    const div = e.target;
-    if(!div.textContent != ""){
-        const index = arrayGrid.indexOf(div);
-        if(isPlayerOne){
-            div.textContent = "X";
-            isPlayerOne = false;
-            gameboard[index] = "x";
+    const getGameboard = ()=>{
+        return gameboard;
+    }
+
+    const reset = ()=>{
+        for(let i=0;i<9;i++){
+            gameboard[i] = 0;
         }
-        else{
-            div.textContent = "O";
-            isPlayerOne = true;
-            gameboard[index] = "o";
-        }
-        if(endGame(gameboard)){
-            if(div.textContent === "X"){
-                result.textContent = "Player one has won";
-            }else{
-                result.textContent = "Player two has won";
+    }
+
+    return{setField , getGameboard ,reset};
+})()
+
+const GameController = (()=>{
+    let round = 0;
+    const winningCombination = [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+
+    const updateRound = ()=>{
+        round++;
+    }
+
+    const resetRound = ()=>{
+        round = 0;
+    }
+
+    const endGame = ()=>{
+        const gameboard = Gameboard.getGameboard();
+        return winningCombination.some((combination)=>{
+                return gameboard[combination[0]]==gameboard[combination[1]] &&
+                gameboard[combination[0]] == gameboard[combination[2]] && 
+                gameboard[combination[0]] !== 0 
+        })
+    }
+
+    const getRoundNumber = ()=>{
+        return round;
+    }
+
+    return{getRoundNumber,endGame,updateRound,resetRound};
+})()
+
+const displayController = (()=>{
+    const grid = document.querySelector('.grid');
+    const cells = document.querySelectorAll('.cell');
+    const result = document.querySelector('#result');
+    const restart = document.querySelector('#restart');
+    let isPlayerOne = true;
+    
+
+    grid.addEventListener('click',(e)=>{
+        const div = e.target;
+        if(!div.textContent != "" && GameController.endGame() == false){
+            GameController.updateRound();
+            
+            const arrayGrid = Array.prototype.slice.call(cells);
+            const index = arrayGrid.indexOf(div);
+
+            if(isPlayerOne){
+                div.textContent = "X";
+                isPlayerOne = false;
+                Gameboard.setField(index,'x');
             }
-        }
-    } 
-})
+            else{
+                div.textContent = "O";
+                isPlayerOne = true;
+                Gameboard.setField(index,'o');
+            }
 
-function endGame(gameboard){
-   for(let i=0;i<=2 ; i++){
-    if(gameboard[0+i] === gameboard[3+i] && gameboard[0+i] === gameboard[6+i] && gameboard[i] !== 0)
-        return true;
-   }
+            if(GameController.endGame()){
+                if(div.textContent === "X"){
+                    result.textContent = "Player one has won";
+                }else{
+                    result.textContent = "Player two has won";
+                }
+            }
 
-   for(let i=0;i<=2 ; i++){
-    if(gameboard[0+3*i] === gameboard[1+3*i] && gameboard[0+3*i] === gameboard[2+3*i] && gameboard[3*i] !== 0)
-        return true;
-   }
+            if(GameController.getRoundNumber() === 9 && !GameController.endGame()){
+                result.textContent="It is a Tie";
+            }
+        } 
+    })
 
-   if(gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8] && gameboard[0] !== 0){
-    return true;
-   }
+    restart.addEventListener('click',()=>{
+        Gameboard.reset();
+        GameController.resetRound();
+        cells.forEach(cell=>{
+            cell.textContent = "";
+        })
+    })
+})()
 
-   if(gameboard[2] === gameboard[4] && gameboard[2] === gameboard[6] && gameboard[2] !== 0){
-    return true;
-   }
 
-}
+
+
+
+
 
 
 
